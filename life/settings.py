8e656d464a171c000095a45513f19d1e71007eca
Alexandre <alexandreaddres@gmail.com>
2023-12-04 14:04:07 -0300

    defining the base of css and static folders

--- life/settings.py
+++ life/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (path.join(BASE_DIR), 'templates/static')
+STATIC_ROOT = path.join(BASE_DIR, 'static')
+MEDIA_ROOT = path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
