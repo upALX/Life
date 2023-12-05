f4590286ac0cccec26451e6e5a336d57e9b6f656
Alexandre <alexandreaddres@gmail.com>
2023-12-05 15:45:10 -0300

    exam app created

--- life/settings.py
+++ life/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth_users'
+    'auth_users',
+    'exams'
 ]
 
 MIDDLEWARE = [
