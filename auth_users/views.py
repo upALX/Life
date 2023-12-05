e01a1a3c8a25ea66d7d9ea634e29a84695f5a0e2
Alexandre <alexandreaddres@gmail.com>
2023-12-05 15:32:01 -0300

    feature login created with no final web view

--- auth_users/views.py
+++ auth_users/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
-# from django.contrib.auth import au
+from django.contrib.auth import authenticate, login
+import traceback
+from time import sleep
 
 # Create your views here.
 def registry(request):
     if request.method == 'GET':
         return render(request, 'registry.html')
     elif request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name') 
+        first_name = request.POST.get('f_name')
+        last_name = request.POST.get('l_name') 
         username = request.POST.get('username')
         email = request.POST.get('email')
         password: str = request.POST.get('password')
@@ -33,15 +35,29 @@ def registry(request):
                     email=email, 
                     password=password
                 )
-                messages.add_message(request, constants.SUCCESS, 'Information saved with success!!!')
-            except:
+                messages.add_message(request, constants.SUCCESS, f'Information of {user.username} saved with success!!!')
+                sleep(2)
+            except Exception as ex:
                 messages.add_message(request, constants.ERROR, 'Internal error of system...')
+                print('DEBUG', traceback.format_exc())
                 return redirect('/users/registry/') 
+            
+            return redirect('/users/login')
 
-def login(request):
+def user_login(request: HttpRequest):
     if request.method == 'GET':
         return render(request, 'login.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
+        user_auth = authenticate(username=username, password=password)
+        print(user_auth)
+        if user_auth:
+            login(request=request, user=user_auth)
+            return redirect('/platform')
+        else:
+            messages.add_message(
+                request=request, 
+                level=constants.ERROR, 
+                message='Username or password invalid. Verify if you was create an account before try to login.')
+            return redirect('/users/login/')
